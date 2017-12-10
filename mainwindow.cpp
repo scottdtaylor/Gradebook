af160300d23e15b8e88e62a2a52abe0c3c7f9da7
Scott <scott.d.taylor@earthlink.net>
2017-12-09 21:42:35 -0800

    In process of grade change

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,13 +11,16 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
     connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), this, SLOT(customslot(int)));
-    connect(this, SIGNAL(newvalue(int)), ui->spinBox, SLOT(setValue(int)));
+    connect(this, SIGNAL(newvalue(int)), ui->label_13, SLOT(setNum(int)));
 }
 
 void MainWindow::customslot(int value){
     emit newvalue(value);
 }
 
+void MainWindow::changegrade(){
+
+}
 MainWindow::~MainWindow()
 {
     delete ui;
--- mainwindow.h
+++ mainwindow.h
@@ -20,9 +20,16 @@ signals:
     void newvalue(int value);
 public slots:
    void customslot(int value);
+   void changegrade();
 
 private:
     Ui::MainWindow *ui;
+   std::vector<int> HW;
+   std::vector<int> Midterms;
+   int Final;
+   std::string Scheme;
+   std::string Class;
+
 
 };
 
